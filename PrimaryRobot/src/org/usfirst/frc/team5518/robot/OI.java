514adab33e83c9ea90e245c2e1838766eaccd8a5
swagle3 <swagle@ncsu.edu>
2016-02-06 17:57:23 -0500

    Button Control

--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/OI.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/OI.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team5518.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 import org.usfirst.frc.team5518.robot.commands.ExampleCommand;
 
 /**
@@ -17,7 +20,7 @@ public class OI {
     
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
+    // commands the same as any other Button.b
     
     //// TRIGGERING COMMANDS WITH BUTTONS
     // Once you have a button, it's trivial to bind it to a button in one of
@@ -26,7 +29,7 @@ public class OI {
     // Start the command when the button is pressed and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenPressed(new ExampleCommand());
-    
+   
     // Run the command while the button is being held down and interrupt it once
     // the button is released.
     // button.whileHeld(new ExampleCommand());
@@ -34,5 +37,15 @@ public class OI {
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
+	Joystick XBOX1 = new Joystick(0);
+	Button button0 = new JoystickButton(XBOX1, RobotMap.XBOX1_ABUTTON);
+	Button button1 = new JoystickButton(XBOX1, RobotMap.XBOX1_BBUTTON);
+	Button button2 = new JoystickButton(XBOX1, RobotMap.XBOX1_XBUTTON);
+	Button button3 = new JoystickButton(XBOX1, RobotMap.XBOX1_YBUTTON);
+	Button button4 = new JoystickButton(XBOX1, RobotMap.XBOX1_RBUMBER);
+	Button button5 = new JoystickButton(XBOX1, RobotMap.XBOX1_LBUMBER);
+	Button button6 = new JoystickButton(XBOX1, RobotMap.XBOX1_BACK);
+	Button button7 = new JoystickButton(XBOX1, RobotMap.XBOX1_START);
+	Button button8 = new JoystickButton(XBOX1, RobotMap.XBOX1_LSTICK);
+	Button button9 = new JoystickButton(XBOX1, RobotMap.XBOX1_RSTICK);
 }
-
--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/RobotMap.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/RobotMap.java
@@ -8,7 +8,22 @@ package org.usfirst.frc.team5518.robot;
 public class RobotMap {
 	
 	// Joystick 0 control mapping
-	public static int XBOX1_LSTICK0 = 0; 
+	public static int XBOX1_LSTICKY = 1; 
+	public static int XBOX1_RSTICKX = 4;
+	public static int XBOX1_LTRIGGER = 2;
+	public static int XBOX1_RTRIGGER = 3;
+	
+	public static int XBOX1_YBUTTON = 3;
+	public static int XBOX1_XBUTTON = 2;
+	public static int XBOX1_ABUTTON = 0;
+	public static int XBOX1_BBUTTON = 1; 
+	public static int XBOX1_RBUMBER = 5;
+	public static int XBOX1_LBUMBER = 4;
+	public static int XBOX1_LSTICK = 8;
+	public static int XBOX1_RSTICK = 9;
+	public static int XBOX1_START = 7;
+	public static int XBOX1_BACK = 2;
+	
 	
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
