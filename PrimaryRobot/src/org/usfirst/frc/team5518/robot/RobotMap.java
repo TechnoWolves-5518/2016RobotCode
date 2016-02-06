6c5847234c21fa1d2ed965dd16477caca6ca5f0e
swagle3 <swagle@ncsu.edu>
2016-02-06 17:12:05 -0500

    Buttons

--- PrimaryRobot/src/org/usfirst/frc/team5518/robot/RobotMap.java
+++ PrimaryRobot/src/org/usfirst/frc/team5518/robot/RobotMap.java
@@ -6,6 +6,10 @@ package org.usfirst.frc.team5518.robot;
  * floating around.
  */
 public class RobotMap {
+	
+	// Joystick 0 control mapping
+	public static int XBOX1_LSTICK0 = 0; 
+	
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
@@ -15,4 +19,5 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+	
 }
